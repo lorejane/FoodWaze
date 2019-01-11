35a6ff54e86697e49fd010b2ccc1845395e9d6be
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-11 14:43:48 -0800

    Update admin.php

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -137,6 +137,27 @@ class Admin extends _BaseController {
         $str .= '"status":"'.($valid ? '1' : '0').'"}';
         echo $str;
     }   
+    
+    public function ValidateCategories(){
+        $category = $this->input->post('category');
+        $str = '{';
+        $valid = true;
+        if(!v::notEmpty()->validate($category['CategoryName'])){
+            $str .= $this->invalid('CategoryName', 'Please input a value');;
+            $valid = false;
+        }
+        else{
+            $ifExist = $this->CategoriesModel->_exist('CategoryName', $category['CategoryName']);            
+            if(is_object($ifExist)){
+                if($ifExist->CategoryId != $category['CategoryId']){
+                    $str .= $this->invalid('CategoryName', 'Category already exist');
+                    $valid = false;
+                }
+            }
+        }
+        $str .= '"status":"'.($valid ? '1' : '0').'"}';
+        echo $str;
+    }
 
 	public function GenerateTableStall(){
         $json = '{ "data": [';
