0344beaf76737736209cde04e7af78ca02cad5f2
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-17 19:34:14 -0800

    Create Payment.php

---
+++ application/views/Cashier/Payment.php
@@ -0,0 +1,27 @@
+<header class="header"> 
+<div class="container">
+	<div class="header-info">
+	<div class="left">
+		<br>
+		<h2 class="header-title"><strong>EMPLOYEES</strong></h2>
+	</div>
+	</div>
+
+	<div class="header-action">
+	<div class="buttons">
+		<a class="btn btn-float btn-lg btn-info float-md-right text-white" href="<?php echo base_url('Cashier/Order'); ?>"
+		data-provide="tooltip">
+		 <span class="close"><i class="ti-close" style="color:white"></i></span>
+		</a>
+	</div>
+	</div>
+</div>
+</header> -->
+	<div class="card">
+		<div class="card-body">
+			
+		</div>
+	</div>
+<!--
+<?php include("EmployeesModal.php");?>
+-->
