39a17742bc68edceb7862d272f7820e3a959cf1c
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-18 16:14:08 -0800

    Create cash_footer.php

---
+++ application/views/include/cash_footer.php
@@ -0,0 +1,31 @@
+<!-- for homepage footer -->
+<footer class="site-footer">
+    <div class="row">
+        <div class="col-md-12">
+        <p class="text-center text-md-left">Copyright © 2018 <a href="#">FoodWaze</a>. All rights reserved.</p>
+        </div>
+
+        <!-- <div class="col-md-6">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+            <a class="nav-link" href="../help/articles.html">Documentation</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="../help/faq.html">FAQ</a>
+            </li>
+        </ul>
+        </div> -->
+
+
+    </div>
+    </footer>
+</main>
+
+<!-- Scripts -->
+<script src = "<?php echo base_url('assets/js/core.min.js'); ?>"></script>
+<script src = "<?php echo base_url('assets/js/app.min.js'); ?>"></script>
+<script src = "<?php echo base_url('assets/js/script.min.js'); ?>"></script>
+<script src = "<?php echo base_url('assets/js/hideshow.js'); ?>"></script>
+
+</body>
+</html>
