358b3dbe6795404b421d936dca320d3bc8ba6cf8
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-19 23:08:44 -0800

    Update CategoriesModal.php

--- application/views/Admin/ManageCategories/CategoriesModal.php
+++ application/views/Admin/ManageCategories/CategoriesModal.php
@@ -27,28 +27,7 @@
         </div>
     </div>
 </div>
-<div class="modal modal-center fade" id="modal-Remove" tabindex="-1">
-    <div class="modal-dialog modal-md ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Are you sure?</h5>
-                <button type="button" class="close" data-dismiss="modal">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body form-type-line">
-                <div class="col-md-12 col-sm-12">
-                    <center>
-                    <form id="modal-Remove-form" action="#" class="form-group mt-2">                                           
-                        <button type="button" class="btn btn-secondary " data-dismiss="modal">No</button>
-                        <button type="button" class="btn btn-danger" onclick="Categories_Modal.delete()">Yes</button>
-                    </form>
-                </center>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 <script>
     var Categories_Modal = {
         data: function () {
@@ -77,49 +56,6 @@
             $('.modal-title').text('Add Category');            
             $('#rowActive').addClass('invisible');
             Categories_Modal.init();
-        },
-
-        edit: function (id) {            
-            $('.modal-title').text('Edit Categories');  
-            $('#rowActive').removeClass('invisible');          
-            Categories_Modal.init();
-            $.ajax({
-                url: "<?php echo base_url('Admin/GetCategory/'); ?>" + id,
-                success: function(i){
-                    i = JSON.parse(i);
-                    console.log(i);
-                    $('#CategoryId').val(i.CategoryId);
-                    $('#CategoryName').val(i.CategoryName);
-                }
-            });           
-        },
-        
-        delete: function (id) {             
-            swal({
-                title: 'Confirm Submission',
-                text: 'Save changes for Employee',
-                type: 'warning',
-                showCancelButton: true,
-                cancelButtonText: 'No! Cancel',
-                cancelButtonClass: 'btn btn-default',
-                confirmButtonText: 'Yes! Go for it',
-                confirmButtonClass: 'btn btn-info'
-            }).then((result) => {
-                if (result.value) {
-                    $.ajax({
-                        url:"<?php echo base_url('Manager/DeleteCategories/'); ?>" +id,
-                            success: function(i){
-                                swal('Deleted!', 'success');
-                                $('#Categories-table').DataTable().ajax.reload();
-                                console.log(i);
-                            }, 
-                            error: function(i){
-                                swal('Oops!', "Something went wrong", 'error');
-                            }
-                    })                                     
-                }
-            })
-
         },
 
         validate: function(){
