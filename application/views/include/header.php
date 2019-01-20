8edc80e156444051c35769c2cc6fa0614bf8542f
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-19 23:09:14 -0800

    Update header.php

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -80,7 +80,28 @@
 </style>
 	</head>
 
-
+<script>
+  function toggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+      if (document.documentElement.requestFullScreen) {  
+        document.documentElement.requestFullScreen();  
+      } else if (document.documentElement.mozRequestFullScreen) {  
+        document.documentElement.mozRequestFullScreen();  
+      } else if (document.documentElement.webkitRequestFullScreen) {  
+        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+      }  
+    } else {  
+      if (document.cancelFullScreen) {  
+        document.cancelFullScreen();  
+      } else if (document.mozCancelFullScreen) {  
+        document.mozCancelFullScreen();  
+      } else if (document.webkitCancelFullScreen) {  
+        document.webkitCancelFullScreen();  
+      }  
+    }  
+  }
+</script>
 <body>
 <?php     
   if($this->session->has_userdata('logged_in')){
