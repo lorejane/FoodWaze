99c8a7128d8e02d32b9b686c0dab615ac3bc28fb
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-01-26 16:54:25 -0800

    Update Order.php

--- application/views/Cashier/Order.php
+++ application/views/Cashier/Order.php
@@ -1,68 +1,37 @@
 <div class="main-content" style="padding-top:5%;">
- <div class="row">
-  <div class="col-sm-8">
-    <div class="card" style="height:90%;">
-         <div class="card-body" >
-            <div id="menu-container">
-            </div>              
-        </div>          
+  <div class="row">
+    <div class="col-sm-4">
+    	<div class="row" style="height:60%;" >
+    		<H3>RECEIPT</H3>
+    		<div class="cart" >
+                <div id="mycart"></div>
+        </div><!-- show cart -->
+
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          SUB TOTAL<input class="input-value" id="input-quantity-'+data.id+'" value='+data.qty+' readonly > <br/>
+          TAX<input class="input-value" id="input-quantity-'+data.id+'" value='+data.qty+' >  <br/>
+          DISCOUNT<input class="input-value" id="input-quantity-'+data.id+'" value='+data.qty+' >  <br/>
+          TOTAL PRICE<input class="input-value" id="input-quantity-'+data.id+'" value='+data.qty+' readonly>
         </div>
-   </div>
-<div class="col-sm-4">
-	<div>
-		<center><H3>RECEIPT</H3></center>
-		<br/>
-		<!--TOTAL:&nbsp;<input type="text" id="input" readonly><br/><br/>
-		 CASH:&nbsp;<input id="idOfInput" type="text"  name="txt1" style="text-align:right;"><br/>
-		CHANGE:&nbsp;<input type="text" id="input"><br/> -->
-		<div class="cart" >
-            <div id="mycart"  ></div>
-    </div><!-- show cart -->
-	</div>
-  <a class="button"  href="<?php echo base_url('Cashier/Payment'); ?>">PAYMENT</a>
-	<!--
-		<div class="row" style="height:45%;">
-		<form name="calcform">
-		<button type="button" name="btn9" value="9" onclick="displaynum(btn9.value)" class="keypad btn btn-default">9</button>
-		<button type="button" name="btn8" value="8" onclick="displaynum(btn8.value)" class="keypad btn btn-default">8</button>
-		<button type="button" name="btn7" value="7" onclick="displaynum(btn7.value)" class="keypad btn btn-default">7</button>
-		<button type="button" name="btn6" value="6" onclick="displaynum(btn6.value)" class="keypad btn btn-default">6</button><br/>
-		<button type="button" name="btn5" value="5" onclick="displaynum(btn5.value)" class="keypad btn btn-default">5</button>
-		<button type="button" name="btn3" value="3" onclick="displaynum(btn3.value)" class="keypad btn btn-default">3</button>
-		<button type="button" name="btn4" value="4" onclick="displaynum(btn4.value)" class="keypad btn btn-default">4</button>
-		<button type="button" name="btn2" value="2" onclick="displaynum(btn2.value)" class="keypad btn btn-default">2</button><br/>
-		<button type="button" name="btn0" value="0" onclick="displaynum(btn0.value)" class="keypad btn btn-default">0</button>
-		<button type="button" name="btn1" value="1" onclick="displaynum(btn1.value)" class="keypad btn btn-default">1</button>
-		<button type="reset" name="reset" class="keypad btn btn-danger">C</button>
-		<button type="button" id="idOfButtonToClick" class="keypad btn btn-danger">X</button><br/>
-		<button type="button" name="eqlbtn" value="=" onclick="txt1.value=eval(txt1.value)" class="keypad btn btn-default">=</button>
-		Amount:<input id="idOfInput" type="text"  name="txt1" style="text-align:right; width:15%;">
-	</form>
-	</div> -->
-</div>
-</div>
-<script>
-	function displaynum(n1){
-    	calcform.txt1.value=calcform.txt1.value+n1;
-  	}
-  	$(document).ready(function(){
-		$('.b').click(function(){
-	        $('#input').val(Number($('#input').val()) + Number($(this).val()));
-	    });
-
-	    $('#idOfButtonToClick').click(function(){
-            var inputString = $('#idOfInput').val();
-            var shortenedString = inputString.substr(0,(inputString.length -1));
-            $('#idOfInput').val(shortenedString);
-        });
-
-  	});
-     	
-</script>
-
+        <div class="col-sm-6">
+          RECEIVED AMOUNT<input class="input-value" id="input-quantity-'+data.id+'" value='+data.qty+' ><br/>
+          <a class="button"  href="<?php echo base_url('Cashier/Payment'); ?>">PAYMENT</a> <br/>
+          <a class="button"  href="<?php echo base_url('Cashier/Payment'); ?>">PAYMENT</a> <br/>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="card" style="height:90%;">
+          <div class="card-body" >
+              <div id="menu-container">
+              </div>              
+          </div>          
+        </div>
+    </div>
+  </div>
  <script>	
-
-
 
 function menu() {
 	$.ajax({
