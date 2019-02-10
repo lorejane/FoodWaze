8df64c6209089320320fcfdf5824e08232c43aee
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-09 18:08:10 -0800

    Update header.php

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -21,24 +21,19 @@
 		<link rel="shortcut icon" href="../favicon.ico"> 
 		<link rel="stylesheet" type="text/css" href="css/default.css" />
 
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
-		<script src="js/modernizr.custom.js"></script>
-   
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/component.css" />   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>FoodWaze</title>
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>">
     <link href="<?php echo base_url('bootstrap/js/jquery.min.js'); ?>">
+    <link href="<?php echo base_url('css/keypad.css' ); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/core.min.css' ); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/app.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/style.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/lorestyle.css' ); ?>" rel="stylesheet">
-    <link rel="stylesheet" href="css/jquery.keypad.css">
-    
 
-    <script src="dist/js/jquery.plugin.min.js"></script>
-    <script src="dist/js/jquery.keypad.js"></script>
     <script src="<?php echo base_url('dist/jspdf.debug.js'); ?>"></script>
     <script src="<?php echo base_url('dist/jspdf.min.js'); ?>"></script>
     <script src = "<?php echo base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script> 
@@ -46,60 +41,7 @@
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.js'); ?>"></script> 
     <script src="<?php echo base_url('bootstrap/js/jquery.min.js'); ?>"></script>
-    <style>
-      .menu{
-        border: 2px solid rgba(0, 0, 0, .2);
-      }
-      .glyphicon {
-        font-size: 100px;
-      }
-      .order {
-        float: left;
-        width: 65px;
-        height: 60px;
-        margin: 5px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      #color_box {
-        width: 30px;
-        height: 30px;
-        margin: 2px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      .click {
-        float: left;
-        width: 135px;
-        height: 120px;
-        margin: 3px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      .value {
-        float: left;
-        width: 25px;
-        height: 25px;
-        margin: 5px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      .keypad {
-        float: left;
-        width: 63px;
-        height: 63px;
-        margin: 5px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      .keypad_op {
-        float: left;
-        width: 50px;
-        height: 50px;
-        margin: 5px;
-        border: 1px solid rgba(0, 0, 0, .2);
-      }
-      label{
-        margin:0;
-        padding:0;
-      }
 
-</style>
 	</head>
 
 <script>
