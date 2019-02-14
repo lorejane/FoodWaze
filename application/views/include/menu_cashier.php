aca778c70fbcadd5402ba49851e34e0462e743a1
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-13 22:10:47 -0800

    Update menu_cashier.php

--- application/views/include/menu_cashier.php
+++ application/views/include/menu_cashier.php
@@ -1,30 +1,25 @@
 
 <header class="topbar topbar-inverse">
     <div class="topbar-left">
-   
-
-    <a class="topbar-btn d-none d-md-block" href="#" data-provide="fullscreen tooltip" title="Fullscreen">
-        <i class="material-icons fullscreen-default">fullscreen</i>
-        <i class="material-icons fullscreen-active">fullscreen_exit</i>
-    </a>
+        <a onclick="toggleFullScreen()" class="topbar-btn d-none d-md-block" href="#" data-provide="fullscreen tooltip" title="Fullscreen">
+            <i class="material-icons fullscreen-default">fullscreen</i>
+        </a>
+        <a href="<?php echo base_url('Cashier/Order'); ?>">FoodWaze</a>
 
     </div>
 
-    <div class="topbar-right">
-        
-
+    <div class="topbar-right">        
+        <div class="dropdown">
+          <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span><i class="ti-user"></i></span> </a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php echo base_url('Cashier/Profile'); ?>">Profile</a></li>
+            <!-- <li><a href="#">CSS</a></li> -->
+            <li><a href="<?php echo base_url('home/Logout'); ?>">Logout<!-- <i class="ti-power-off" style="font-size:20; color:red;"></i> --></a></li>
+          </ul>
+        </div>
         <li class=" d-md-block">
             <span class="topbar-btn has-new" data-toggle="quickview" data-target="#qv-pending"><i class="ti-briefcase"></i></span>        
         </li>
-
-<div class="dropdown">
-  <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span><i class="ti-briefcase"></i></span> </a>
-  <ul class="dropdown-menu">
-    <li><a href="#">CSS</a></li>
-    <li><a href="#">JavaScript</a></li>
-    <li><a href="<?php echo base_url('home/Logout'); ?>"><i class="ti-power-off" style="font-size:20; color:red;"></i></a>        </li>
-  </ul>
-</div>
     </div>
 
 <!-- Notifications -->
@@ -45,7 +40,7 @@
         <!-- <a class="btn btn-outline btn-info" href="<?php echo base_url('Bookbag/'); ?>">View Detailed Bookbag</a> -->
     </div>
     <div>
-        <button class="btn btn-outline btn-danger" onclick="Bookbag.removeAll()" data-provide="tooltip" title="Remove all"><i class="fa fa-2x fa-trash"></i></>        
+        <button class="btn btn-outline btn-danger" onclick="deleteAllpending()" data-provide="tooltip" title="Remove all"><i class="fa fa-2x fa-trash"></i></>        
     </div>
     </footer>
 </div>
@@ -55,10 +50,10 @@
 <script>
     $(document).ready(function(){
         Message.refresh();
-        // window.setInterval(function(){
-        //  Message.refresh();
-        //  console.log("hello");
-        // }, 2500)
+        window.setInterval(function(){
+            Message.refresh();
+            console.log("hello");
+        }, 2500)
     });
 
     var Message = {
@@ -70,52 +65,67 @@
                     $('#orders').empty();
                     if(i != '{}'){
                         i = JSON.parse(i);
-                        console.log(i);
                         $.each(i, function(index, data){                            
                             $('#orders').append(
-                                '<a class="orderx media media-new" data-value="'+data.OrderId+'" href="#">'
+                                // '<a class="orderx media media-new" data-value="'+data.OrderId+'" href="#">'
+                                '<a class="media media-new" href="#">'
                                     + '<div class="media-body">'
                                         + '<p>' + data.OrderId + '</p>'                                        
                                         + '<p>' + data.Name + '</p>'                                        
-                                        + '<p>' + data.DateTime + '</p>'                                        
+                                        + '<p>' + data.DateTime + '</p>'
+                                        + '<p><i class="orderx btn-info btn-xs fa fa-edit right"  data-value="'+data.OrderId+'"></i> <i class="btn btn-warning btn-xs fa fa-print right" onclick="minus1('+data.Id+')" id="'+data.OrderId+'"></i> <i class="btn btn-danger btn-xs fa fa-trash right" onclick="deletepending('+data.OrderId+',this)" id="'+data.OrderId+'"></i></p>'                                        
                                     + '</div>'                                        
                                 + '</a>'
                             );
                         });
                         $('.orderx').click(function(){
+                            // $.ajax({
+                            //     url: "<?php echo base_url('Cashier/RemoveAll'); ?>",
+                            //     success: function(i){
+                                    
+                            //     }
+                            // });
+                            deleteAll();
+
+                            var element ='<table class="table-responsive table-hover"  style="height:50%;"> <thead> <tr>  <th>Qty</th>  <th>Name</th> <th>Price</th> <th>Total</th> <th></th> <th>Action</th> <th></th> </tr> </thead> <tbody></tbody></table>';
+                            $("#mycart").html(element);
                             $.ajax({
                                 url: "<?php echo base_url('Cashier/GetOrderDetails'); ?>",
                                 data: {orderid:$(this).data("value")},
                                 type: "POST",
                                 success: function(j){
-                                    total = 0;
-                                    var element = '';
-                                    $("#mycart").html('<table class= "table"> <thead> <tr> <th>Name</th> <th>Qty</th> <th>Price</th> <th>Total</th> <th></th> </tr> </thead>');
-                                    $("#mycart").append('<tbody>');
                                     j = JSON.parse(j);
-                                    console.log(j);
+                                    console.table(j);
                                     $.each(j, function(index, data){
+                                        var total= 0;
                                         $.ajax({
                                             url: "<?php echo base_url('Cashier/GetMenu/'); ?>" +data.MenuId,
                                             success: function(k){
                                                 k = JSON.parse(k);
-                                                console.log(k);
-                                                console.log('x ' + data.Quantity + " " + k[0].Name);
-                                                var xelement = '<tr>'
-                                                +'<td>'+k[0].Name+'</td>'
-                                                +'<td>'+data.Quantity+'</td>'
-                                                +'<td>'+k[0].Price+'</td>'
-                                                +'<td>'+(data.Quantity * k[0].Price)+'</td>'
-                                                +'<td><i class="btn btn-warning btn-xs fa fa-close right" onclick="minus1('+data.Id+')" id="'+data.Id+'"></i></td> <td> <i class="btn btn-danger btn-xs fa fa-trash right" onclick="deletecart('+data.Id+')"id="'+data.Id+'"></i><td> </tr>';
-                                                +'</tr>';
-                                                total = Number(total) + Number(data.Quantity * k.Price);
-                                                // console.log(element);
-                                            $("#mycart").append(xelement);
+                                                console.table(k);
+                                                $.ajax({
+                                                    url: "<?php echo base_url('Cashier/AddToCart'); ?>",
+                                                    type: "POST",
+                                                    data: {
+                                                        Order: {
+                                                            Image: k[0].Image,
+                                                            id: k[0].MenuId,
+                                                            name: k[0].Name,
+                                                            qty: data.Quantity, 
+                                                            price: k[0].Price
+                                                        }
+                                                    },
+                                                    success: function(j){
+                                                        console.log(j);
+                                                        console.log("hHEHEEH");
+                                                    }
+                                                })
+                                                var elementx =' <tr>  <td><input class="input-quantity" id="input-quantity-'+data.id+'" value='+data.Quantity+'  readonly></td> <td>'+k[0].Name+'</td> <td>'+k[0].Price+'</td> <td>'+(data.Quantity * k[0].Price)+'</td> <td><div id="cart-price-'+data.id+'">'+(data.Quantity * k[0].Price)+'</div></td>  <td><div class="btn-increment-decrement" onClick="decrement_quantity('+data.id+', '+k[0].Price+')">-</div><div class="btn-increment-decrement" onClick="increment_quantity('+data.id+', '+k[0].Price+')">+</div></td> <td> <i class="btn btn-danger btn-xs fa fa-trash right" onclick="DeleteCart(\''+data.rowid+'\')" id="'+data.id+'"></i><td></tr> ';
+                                                total = Number(total) + Number(data.Quantity * k[0].Price);        
+                                                $("#mycart table tbody").append(elementx);
                                             }                       
-                                        })
-                                    })
-
-                                    $("#mycart").append('</tbody></table><p> Total: '+total+' </p>');                                        
+                                        });
+                                    });    
                                 }
                             })              
                         });
@@ -127,5 +137,83 @@
 
     }
 
+
+function DeleteCart(id){
+  console.log(id);
+  $.ajax({
+      url: "<?php echo base_url('Cashier/Remove/');?>" + id,
+      success: function(i){
+        refresh();
+    }
+  });
+}   
+
+
+    function deletepending(id,dis){   
+            swal({
+                title: 'Confirm Submission',
+                text: 'Save changes for Pending Order',
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonText: 'No! Cancel',
+                cancelButtonClass: 'btn btn-default',
+                confirmButtonText: 'Yes! Go for it',
+                confirmButtonClass: 'btn btn-info'
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        url:"<?php echo base_url('Cashier/DeletePendingOrders/'); ?>" +id,
+                            success: function(i){
+                                swal('Deleted!', 'success');
+                                //$('#Menu-table').DataTable().ajax.reload();
+                                console.log(i);
+                                var mwen = $(dis).fadeOut(500, function() { $(dis).closest("a").remove(); });
+                            }, 
+                            error: function(i){
+                                swal('Oops!', "Something went wrong", 'error');
+                            }
+                    })                                     
+                }
+            })
+
+        }
+
+    function deleteAllpending(){   
+            swal({
+                title: 'Confirm Submission',
+                text: 'Save changes for Pending Order',
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonText: 'No! Cancel',
+                cancelButtonClass: 'btn btn-default',
+                confirmButtonText: 'Yes! Go for it',
+                confirmButtonClass: 'btn btn-info'
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        url:"<?php echo base_url('Cashier/DeleteAllPendingOrders/'); ?>" ,
+                            success: function(i){
+                                swal('Deleted!', 'success');
+                                //$('#Menu-table').DataTable().ajax.reload();
+                                console.log(i);
+                              //  var mwen = $(dis).fadeOut(500, function() { $(dis).closest("a").remove(); });
+                            } 
+                            // error: function(i){
+                            //     swal('Oops!', "Something went wrong", 'error');
+                            // }
+                    })                                     
+                }
+            })
+
+        }
+
+    function deleteAll(){
+        $.ajax({
+            url: "<?php echo base_url('Cashier/RemoveAll'); ?>",
+            success: function(i){
+
+            }
+        });
+    }
 
 </script>
