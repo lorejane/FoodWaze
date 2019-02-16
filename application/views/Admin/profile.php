c9d622e53183c05b30b2b82b8c0a89cd29d91629
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-15 18:13:36 -0800

    Update profile.php

--- application/views/Admin/profile.php
+++ application/views/Admin/profile.php
@@ -10,13 +10,13 @@
   </div>
 </header>
 
-
 <div class="col-md-8">
               <form class="card">
               <h4 class="card-title"><strong>Edit Profile</strong></h4>
 
               <div class="card-body row">
-
+                <form id="modal-Employee-form" action="#" class="form-group mt-2">
+                       <input type="hidden" id="EmployeeId" name="EmployeeId" />   
                             <div class="col-6">
                                 <label>Account</label>
                                 <input id="EmployeeAccount" name="EmployeeAccount" type="text" class="form-control" placeholder="Account" value="<?php echo $profile->EmployeeAccount ?>" />
@@ -37,14 +37,7 @@
                                 <label>Last Name</label>
                                 <input id="Lastname" name="Lastname" type="text" class="form-control" placeholder="Last Name" value="<?php echo $profile->Lastname ?>"/>
                             </div>
-                        
-                    <!-- selectpicker -->
-                    
-                            <!-- <div class="col-12">
-                                <label>Stall</label>
-                                <select id="StallId" name="StallId" data-provide="selectpicker" title="Choose Stall" data-live-search="true" class="form-control show-tick"></select>
-                            </div>   -->                                     
-                    
+                </form>    
                 </div>
               <footer class="card-footer text-right">
                 <button class="btn btn-secondary" type="reset">Cancel</button>
@@ -55,10 +48,142 @@
 
 <div class="col-md-3 bg-pale-danger b-2 border-danger">
   <div class="sidebar-profile">
-            <a href="<?php echo base_url('Admin/Profile'); ?>"><img class="avatar" src='<?php echo $profile->Image ?>'  alt="..."></a>
+            <input id="image" name="image" type="file" data-provide="dropify" data-show-remove="false" data-default-file="<?php echo base_url("pics/default.png"); ?>" style="border: solid black 1px;">
           <div class="profile-info">
             <h4>Maryam Amiri</h4>
             <p>Web developer</p>
           </div>
 </div>
 </main>
+
+
+<script>
+    var imageChanged = false;
+
+    $(document).ready(function(){
+        $("#image").change(function(event){                     
+            var tgt = event.target || window.event.srcElement, files = tgt.files;       
+            var fr = new FileReader();
+            fr.onload = function(){
+                $("#imgDisplay").children('img').attr('src', fr.result);
+                imageChanged = true;
+            }
+            fr.readAsDataURL(files[0]);
+        });
+    });
+
+
+    var Employee_Modal = {
+        data: function () {
+            return {
+                EmployeeId: $('#EmployeeId').val(),                
+                EmployeeAccount: $('#EmployeeAccount').val(),              
+                Firstname: $('#Firstname').val(),
+                Lastname: $('#Lastname').val(),
+                Password: $('#Password').val()
+            }
+        },         
+       edit: function (id) {            
+            $('.modal-title').text('Edit Employee');  
+            $('#rowActive').removeClass('invisible');          
+            $.ajax({
+                url: "<?php echo base_url('Admin/Get/'); ?>" + id,
+                success: function(i){
+                    i = JSON.parse(i);
+                    console.log(i);
+                    $('#EmployeeId').val(i.EmployeeId);
+                    $('#EmployeeAccount').val(i.EmployeeAccount);
+                    $('#Firstname').val(i.Firstname);
+                    $('#Lastname').val(i.Lastname);
+                    $('#PositionId').selectpicker('val',i.PositionId);
+                    $('#StallId').selectpicker('val',i.StallId);
+                    $('#Password').val(i.Password);
+                    $('#image').parent().find('.dropify-preview .dropify-render img').attr('src', "<?php echo base_url('pics/'); ?>" + i.Image);
+                    imageChanged = false;
+                }
+            });           
+        },
+
+        validate: function(){
+            $('.invalid-feedback').remove();
+            $('.is-invalid').removeClass('is-invalid');
+            $.ajax({
+                url:'<?php echo base_url('Admin/Validate'); ?>',
+                type: "POST",
+                data: {"employee": Employee_Modal.data()},
+                success: function(i){
+                    i = JSON.parse(i);                    
+                    if(i.status == 1){
+                        Employee_Modal.save();
+                    }else{
+                        $.each(i, function(element, message){
+                            if(element != 'status'){
+                                $('#' + element).addClass('is-invalid').parent().append(message);
+                            }
+                        });
+                    }
+                }, 
+                error: function(i){
+                    swal('Oops!', "Something went wrong", 'error');
+                }
+            })      
+        },        
+
+         upload: function(){         
+            var formData = new FormData($('#modal-Employee-form')[0]);            
+            $.ajax({
+                url: "<?php echo base_url("Admin/UploadProfile"); ?>",
+                type: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(data){
+                    console.log('upload: ' + data);                 
+                },
+                error: function(data){
+                    console.log('upload: ' + data);
+                }
+            });
+        },
+
+        save: function () {
+            var message;            
+            if ($('#EmployeeId').val() == 0) {
+                message = "Great Job! New Employee has been created";
+            } else {
+                message = "Nice! Employee has been updated";
+            }
+
+            swal({
+                title: 'Confirm Submission',
+                text: 'Save changes for Employee',
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonText: 'No! Cancel',
+                cancelButtonClass: 'btn btn-default',
+                confirmButtonText: 'Yes! Go for it',
+                confirmButtonClass: 'btn btn-info'
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        url:'<?php echo base_url('Admin/Save'); ?>',
+                        type: "POST",
+                        data: {"employee": Employee_Modal.data()},
+                        success: function(i){
+                             if(imageChanged){                               
+                            Employee_Modal.upload();
+                            }
+                            swal('Good Job!', message, 'success');
+                            $('#modal-Employee').modal('hide');
+                            console.log(i);
+                        }, 
+                        error: function(i){
+                            swal('Oops!', "Something went wrong", 'error');
+                        }
+                    })                                     
+                }
+            })
+        }      
+}
+
+</script>
