4428bc3e55d149ecf4345b5210ef995cdd5ae8a8
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-19 01:11:21 -0800

    Update menu_cashier.php

--- application/views/include/menu_cashier.php
+++ application/views/include/menu_cashier.php
@@ -4,19 +4,14 @@
         <a onclick="toggleFullScreen()" class="topbar-btn d-md-block">
           <button class="btn btn-square btn-outline btn-yellow "> <i class="ti-fullscreen"></i></button>
         </a>
-        <a><img src="../pics/foodwazelogoo.png" alt="logo icon"></a>
-        <span class="sidebar-toggle-fold"></span>
-
-         
-         
-
+        <a  href="<?php echo base_url('Cashier/Order'); ?>"><img src="../pics/foodwazelogoo.png" alt="logo icon"></a>       
     </div>
 
     <div class="topbar-right">        
        <div class="dropdown">
                 <span class="topbar-btn" data-toggle="dropdown"><img class="avatar avatar-sm" src="../pics/logo.png" alt="..."></span>
                 <div class="dropdown-menu dropdown-menu-right">
-                  <a class="dropdown-item" href="<?php echo base_url('manager/Profile'); ?>"><i class="ti-user"></i> Profile</a>
+                  <a class="dropdown-item" href="<?php echo base_url('Cashier/Profile'); ?>"><i class="ti-user"></i> Profile</a>
                   <a class="dropdown-item" href="<?php echo base_url('Home/Logout'); ?>"><i class="ti-power-off"></i> Logout</a>
                 </div>
               </div>
