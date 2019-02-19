8a6e6a960dd1de2dc44b5128182e9ab6e972d188
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-18 22:42:00 -0800

    Create Sales.php

---
+++ application/views/Manager/Sales.php
@@ -0,0 +1,37 @@
+<main class="main-container">
+
+      <div class="main-content">
+        <div class="row">
+
+          
+
+
+            <div class="card">
+              
+
+            <table class="table table-responsive table-bordered display nowrap" id = "Employee-table" style="width:100%; overflow-x:auto;" 
+            cellspacing="0" data-provide = "datatables" data-ajax = "<?php echo base_url("Manager/GenerateReceiptTbl") ?>">
+              <thead>
+                <tr>
+                  <th>Order ID</th>
+                  <th>Employee ID</th>
+                   <th>Total</th>
+                  <th>Cash</th>
+                   <th>Change</th>
+                  <th>Discount</th>
+                   <th>Date/Time</th>
+                  
+                </tr>
+              </thead>
+            </table>
+
+      </div>
+
+            </div>
+          
+
+
+</div>
+</div>
+
+
