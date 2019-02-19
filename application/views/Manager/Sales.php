ace324098beb2f6d6d4479a0fca787a00a181ee2
Pancaaaaake <43904307+Pancaaaaake@users.noreply.github.com>
2019-02-18 18:03:08 -0800

    sales

--- application/views/Manager/Sales.php
+++ application/views/Manager/Sales.php
@@ -5,11 +5,34 @@
 
           
 
+         <div class="col-md-6 col-xl-6">
+            <div class="card card-body bl-3 border-danger">
+              <h6 class="text-uppercase text-gray">Total Sales</h6>
+              <div class="flexbox mt-2">
 
-            <div class="card">
-            <div class="form-row">  
-                <div class="form-group col-md-5 col-sm-12" data-provide="datepicker" data-date-format="yyyy-mm-dd">
-                    <label>From</label>
+                <span class="fa fa-rub text-danger fs-50"></span>
+                <p class="fs-50 fw-100"><?php echo $totalprice ?></p>
+              </div>
+            </div>
+          </div>
+
+           <div class="col-md-6 col-xl-6">
+            <div class="card card-body bl-3 border-danger">
+              <h6 class="text-uppercase text-gray">Total Orders</h6>
+              <div class="flexbox mt-2">
+
+                <span class="fa fa-cart-plus text-danger fs-50"></span>
+                <p class="fs-50 fw-100"><?php echo $totalorders ?></p>
+              </div>
+            </div>
+          </div>
+
+
+        <div class="col-md-12">
+            <div class="card card-body bl-3 border-yellow">
+             <div class="form-row">  
+                <div class="form-group col-md-5 col-sm-12 text-info fs-50" data-provide="datepicker" data-date-format="yyyy-mm-dd">
+                    <label><strong>From</strong></label>
                     <input class="form-control" type="text" id="range-issue-from" name="range-issue-from" placeholder="">
                 </div>
 
@@ -21,22 +44,45 @@
                 <div class="col-md-2 col-sm-12 dash-filter" style="margin-bottom: 30px;">
                     <label>&nbsp;</label>
                     <button class="btn btn-block btn-info" id="filter" onclick="sales.filter()">Filter</button>
-                </div>				
+                </div>              
             </div>
-            <div id="table-container">
-                <table class="table table-responsive table-bordered display nowrap" id = "Employee-table" style="width:100%; overflow-x:auto;" cellspacing="0" data-provide = "datatables" data-ajax = "<?php echo base_url("Manager/GenerateReceiptTbl") ?>">
-                    <thead>
-                        <tr>
-                            <th>Employee</th>
-                            <th>Order ID</th>
-                            <th>Earnings</th>                            
-                            <th>Date of transaction</th>                        
+
+                <div id="table-container">
+                <div class="table-responsive" style="padding-top:20px; ">
+                        <table class="table table-bordered display nowrap fs-20 text-dark" id = "Stall-table" style="width:100%; overflow-x:auto;" 
+                        cellspacing="0" data-provide = "datatables" data-ajax = "<?php echo base_url("Manager/GenerateReceiptTbl") ?>">
+                            <thead>
+                            <tr style="background-color: #0f0a0a;">
+                            <th class="text-yellow">Employee</th>
+                            <th class="text-yellow">Order ID</th>
+                            <th class="text-yellow">Due Amount per Transaction</th>                  
+                            <th class="text-yellow">Date of transaction</th>                        
                         </tr>
                     </thead>
                 </table>
             </div>
+        </div>
+        </div>
+    </div>
+            <div class="col-md-12">
+            <div class="card card-body bl-3 border-yellow">
+             
+             <table class="table table-bordered display nowrap fs-20 text-dark" id = "Stall-table" style="width:100%; overflow-x:auto;" 
+                        cellspacing="0" data-provide = "datatables" data-ajax = "<?php echo base_url("Manager/GenerateTotalByEmployee") ?>"><thead>
+                <tr style="background-color: #0f0a0a;">
+                  <th class="text-yellow">Employee ID</th>
+                  <th class="text-yellow">Name</th>
+                  <th class="text-yellow">Sales</th>
+                </tr>
+              </thead>
+            </table>  
 
-		</div>
+            </div>
+            </div>   
+
+      </div>
+
+            </div>
 
 	</div>
           
