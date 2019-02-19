dc28f823bd5cd33fdb20706c89b3e415d01c280f
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-18 22:42:07 -0800

    Update Order.php

--- application/views/Cashier/Order.php
+++ application/views/Cashier/Order.php
@@ -1,8 +1,4 @@
 <style>
-#SML{
-  display: none;
- } 
-
 .int{
   width: 20%;
 }
@@ -39,44 +35,52 @@
       </div>
       <!-- <br/> -->
       <div class="row" id="customers" style="border: 3px dotted rgba(0, 0, 0, .2);">
-        <div class="col-sm-6" >
-          <!-- SUB TOTAL<input class="input-value" id="input-quantity-'+data.id+'" value='' readonly > <br/>
- -->      
+        <div class="col-sm-8" >
+        <div class="row" style="height:5%; padding:5%;">
         DISCOUNT 
           <select type="text" name="discount" id="discount">
             <option value="0" selected>Regular</option>
             <option value="20">Senior</option>
             <option value="20">PWD</option>
-          </select><br/>
+          </select>
+        </div>
+        <div class="row" style="height:5%; padding:5%;">
           TOTAL PRICE<input type="text" class="int input-value" id="puretotal" name="puretotal" readonly>
+          </div>
+        <div class="row" style="height:5%; padding:5%;">
+          CASH<input type="text"  name="ReceivedAmnt" style="text-align:right;" class="int input-value" id="ReceivedAmnt" >
+          </div>
+        <div class="row" style="height:5%; padding:5%;">
+          CHANGE<input type="text" class="int input-value" name="change" onclick="calculate()" id="change"  readonly/>
+          </div>
+        <div class="row" style="height:5%; padding:5%;">
           <input id="pdf" type="button" class="btn btn-info" onclick="SaveOrder()" value="Save"/>
           <!-- <input id="pdf" type="submit" value="hjkasdhs"/>  --> 
         </div>
-        <div class="col-sm-6">
+      </div>
+        <div class="col-sm-4" style="padding:1%;">
           <div class="row">
                 <!-- <form name="calcform"> -->
                   <div class="row">
-                    <button type="button" name="btn9" value="9" onclick="displaynum(btn9.value)" class="keypad btn btn-default">9</button>
-                    <button type="button" name="btn8" value="8" onclick="displaynum(btn8.value)" class="keypad btn btn-default">8</button>
                     <button type="button" name="btn7" value="7" onclick="displaynum(btn7.value)" class="keypad btn btn-default">7</button>
+                    <button type="button" name="btn8" value="8" onclick="displaynum(btn8.value)" class="keypad btn btn-default">8</button>
+                    <button type="button" name="btn9" value="9" onclick="displaynum(btn9.value)" class="keypad btn btn-default">9</button>
+                    </div>
+                    <div class="row">
+                    <button type="button" name="btn4" value="4" onclick="displaynum(btn4.value)" class="keypad btn btn-default">4</button>
+                    <button type="button" name="btn5" value="5" onclick="displaynum(btn5.value)" class="keypad btn btn-default">5</button>
                     <button type="button" name="btn6" value="6" onclick="displaynum(btn6.value)" class="keypad btn btn-default">6</button>
                     </div>
                     <div class="row">
-                    <button type="button" name="btn5" value="5" onclick="displaynum(btn5.value)" class="keypad btn btn-default">5</button>
-                    <button type="button" name="btn3" value="3" onclick="displaynum(btn3.value)" class="keypad btn btn-default">3</button>
-                    <button type="button" name="btn4" value="4" onclick="displaynum(btn4.value)" class="keypad btn btn-default">4</button>
+                    <button type="button" name="btn1" value="1" onclick="displaynum(btn1.value)" class="keypad btn btn-default">1</button>
                     <button type="button" name="btn2" value="2" onclick="displaynum(btn2.value)" class="keypad btn btn-default">2</button>
+                    <button type="button" name="btn3" value="3" onclick="displaynum(btn3.value)" class="keypad btn btn-default">3</button>
                     </div>
                     <div class="row">
                     <button type="button" name="btn0" value="0" onclick="displaynum(btn0.value)" class="keypad btn btn-default">0</button>
-                    <button type="button" name="btn1" value="1" onclick="displaynum(btn1.value)" class="keypad btn btn-default">1</button>
                     <button type="reset" name="reset" class="keypad btn btn-danger">C</button>
                     <button type="button" id="idOfButtonToClick" class="keypad btn btn-danger">X</button>
                     </div>
-                    <div class="row">
-                    CASH<input type="text"  name="ReceivedAmnt" style="text-align:right;" class="int input-value" id="ReceivedAmnt" >
-                    CHANGE<input type="text" class="int input-value" name="change" onclick="calculate()" id="change"  readonly/>
-                  </div>
           </div>
         </div>
          
@@ -90,18 +94,6 @@
               <div id="menu-container" >
               </div>              
           </div>          
-        </div>
-          <button type="button" style="width:5%" id="idOfButtonToClick"  onclick="Sizes()" class="btn btn-info">+</button>
-        <div id="SML" class="row">
-          <div class="col-sm-4">
-          <button type="button" id="idOfButtonToClick" class="btn btn-default">Small</button><br/>
-          </div>
-          <div class="col-sm-4">
-          <button type="button" id="idOfButtonToClick" class="btn btn-default">Medium</button><br/>
-          </div>
-          <div class="col-sm-4">
-          </div>
-          <button type="button" id="idOfButtonToClick" class="btn btn-default">Large</button><br/>
         </div>
     </div>
   </div>
@@ -130,8 +122,9 @@ $(document).ready(function () {
         url:"<?php echo base_url('Cashier/SaveOrder'); ?>/"+discount+"/"+puretotal+"/"+ReceivedAmnt+"/"+change,
         type: "get",
         success: function(i){
-          //swal('DONE!', 'success');
-          Remove();
+              //alert('rere');
+                $('#mycart').html("");
+                Remove();
           }
     })    
   }
@@ -156,10 +149,6 @@ $(document).ready(function () {
       
 </script>
   <script>
-   
-function Sizes() {
-  $("#SML").toggle()
-}	
 function menu() {
 	$.ajax({
         url: "<?php echo base_url('FoodWaze/GetCategory/'.$this->session->userdata('StallId')); ?>",        
@@ -399,8 +388,6 @@ $('#pdf').click(function () {
   }
 
   var pdf = new jsPDF();
-  pdf.text(90, 5, 'FOODWAZE');
-  pdf.text(5, 10, 'Receipt');
   pdf.text(5, 15, menu);
   pdf.text(5, 70, '------------------');
   pdf.text(5, 85, 'VATable: '+ VATable);
@@ -410,6 +397,13 @@ $('#pdf').click(function () {
   pdf.text(5, 105, 'Total: '+ TotalDue );
   pdf.text(5, 119, 'Cash: '+ Cash);
   pdf.text(5, 115, 'Change: '+ Change);
+
+  pdf.setProperties({
+    title: 'FOODWAZE',
+    sucject: 'This shit is a Receipt',
+    author: 'FoodWaze'
+  });
+
   pdf.save('Receipt.pdf');
   console.log(menu);
   }
