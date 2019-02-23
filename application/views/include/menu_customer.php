3f98ee81fc2b6de57c6fbeea0e83971f8cc75504
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-23 11:47:55 -0800

    Update menu_customer.php

--- application/views/include/menu_customer.php
+++ application/views/include/menu_customer.php
@@ -1,27 +1,23 @@
 
+<header style="background-color: #d02020" class="topbar">
+    <div class="topbar-left">
+        <a onclick="toggleFullScreen()" class="topbar-btn d-md-block">
+          <button class="btn btn-square btn-outline btn-yellow "> <i class="ti-fullscreen"></i></button>
+        </a>
+        <a  href="<?php echo base_url('Customer/Homepage'); ?>"><img src="../pics/foodwazelogoo.png" alt="logo icon"></a>       
+    </div>
 
+    <div class="topbar-right">        
+       <div class="dropdown">
+                <span class="topbar-btn" data-toggle="dropdown"><img class="avatar avatar-sm" src="../pics/logo.png" alt="..."></span>
+                <div class="dropdown-menu dropdown-menu-right">
+                  <a class="dropdown-item" href="<?php echo base_url('Customer/Profile'); ?>"><i class="ti-user"></i> Profile</a>
+                  <a class="dropdown-item" href="<?php echo base_url('Customer/Receipt'); ?>"><i class="ti-user"></i> Profile</a>
+                  <a class="dropdown-item" href="<?php echo base_url('Home/Logout'); ?>"><i class="ti-power-off"></i> Logout</a>
+                </div>
+              </div>
+    </div>
+</header>
 
-
-            <div class="menu-item active">
-                <a class="menu-link" href="<?php echo base_url('homepage'); ?>">
-                    <span class="icon fa fa-home"></span>
-                    <span class="title">Home</span>
-                </a>
-            </div>            
-            <div class="menu-item active">
-                <a class="menu-link" href="<?php echo base_url('customer/orderpage'); ?>">
-                    <span class="icon fa fa-user"></span>
-                    <span class="title">Order</span>
-                </a>
-            </div>            
-
-            <div class="menu-item">
-                <a class="menu-link" href="<?php echo base_url('customer/aboutpage'); ?>">
-                    <span class="icon fa fa-home"></span>
-                    <span class="title">About</span>
-                </a>
-            </div>    
-
-   
-
-
+<script>
+</script>
