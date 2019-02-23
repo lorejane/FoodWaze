813e5e47bc7e77133e507bdfc7ccb857f13a5e52
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-23 11:47:59 -0800

    Create menu_custome.php

---
+++ application/views/include/menu_custome.php
@@ -0,0 +1,27 @@
+
+
+
+
+            <div class="menu-item active">
+                <a class="menu-link" href="<?php echo base_url('homepage'); ?>">
+                    <span class="icon fa fa-home"></span>
+                    <span class="title">Home</span>
+                </a>
+            </div>            
+            <div class="menu-item active">
+                <a class="menu-link" href="<?php echo base_url('customer/orderpage'); ?>">
+                    <span class="icon fa fa-user"></span>
+                    <span class="title">Order</span>
+                </a>
+            </div>            
+
+            <div class="menu-item">
+                <a class="menu-link" href="<?php echo base_url('customer/aboutpage'); ?>">
+                    <span class="icon fa fa-home"></span>
+                    <span class="title">About</span>
+                </a>
+            </div>    
+
+   
+
+
