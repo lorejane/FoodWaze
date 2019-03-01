03ce549d56c9ac9dd26639ad96b42ef1f0fc3349
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-02-28 21:15:03 -0800

    Update Receipt.php

---
+++ application/views/Customer/Receipt.php
@@ -0,0 +1,10 @@
+      <div class="table-responsive" style="padding-top:10px;">
+      <table class="table table-lg table-bordered" cellspacing="0" data-provide = "datatables" data-ajax = "<?php echo base_url("Customer/GenerateReceipts") ?>">
+    <thead>
+      <tr>
+        <th>Order Id</th>
+        <th>Date/Time</th>
+      </tr>
+    </thead>
+  </table> 
+      </div>
