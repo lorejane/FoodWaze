58c3e447ef064ab3dc064022be3b082eb2fc925d
LoreReyes <30516891+lorejane@users.noreply.github.com>
2019-03-03 18:13:26 -0800

    Update ReceiptModal.php

--- application/views/Customer/ReceiptModal.php
+++ application/views/Customer/ReceiptModal.php
@@ -10,11 +10,12 @@
             <div class="modal-body form-type-line">
                 <div class="col-md-12 col-sm-12">
                     <form id="modal-Receipt-form" action="#" class="form-group mt-2">
-                        <input type="hidden" id="OrderId" name="OrderId" />   
+                        <input type="hidden" id="OrderId" name="OrderId" />
+                        <input type="hidden" id="MenuId" name="MenuId" />   
                         <div class="row mb-2">
                             <div class="col-12">
                                 <label>Name</label>
-                                <input id="MenuId" name="MenuId" type="text" class="form-control" placeholder="Name" />
+                                <input id="Name" name="Name" type="text" class="form-control" placeholder="Name" />
                             </div>
                         </div>
                         <div class="row mb-2">
@@ -25,25 +26,25 @@
                         </div> 
                         <div class="row mb-2">
                             <div class="col-12">
-                                <label>Name</label>
+                                <label>Amount Due</label>
                                 <input id="Total" name="Total" type="text" class="form-control" placeholder="Name" />
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-12">
-                                <label>Name</label>
+                                <label>Cash</label>
                                 <input id="Cash" name="Cash" type="text" class="form-control" placeholder="Name" />
                             </div>
                         </div> 
                         <div class="row mb-2">
                             <div class="col-12">
-                                <label>Price</label>
+                                <label>Discount</label>
                                 <input id="Discount" name="Discount" type="text" class="form-control" placeholder="Price" />
                             </div>
                         </div>   
                         <div class="row mb-2">
                             <div class="col-12">
-                                <label>Price</label>
+                                <label>Change</label>
                                 <input id="Change" name="Change" type="text" class="form-control" placeholder="Price" />
                             </div>
                         </div>                                                                                                   
@@ -69,6 +70,7 @@
                 Discount: $('#Discount').val(), 
                 Change: $('#Change').val(),
                 MenuId: $('#MenuId').val(), 
+                Name: $('#Name').val(), 
                 Quantity: $('#Quantity').val()
             }
         },
@@ -98,12 +100,22 @@
                 }
             });
               $.ajax({
-                url: "<?php echo base_url('Customer/GetReceipts/'); ?>" + id,
+                url: "<?php echo base_url('Cashier/GetMenu/'); ?>" + id,
                 success: function(i){
                     i = JSON.parse(i);
                     console.log("edit"); 
                     console.log(i);
                     $('#MenuId').val(i.MenuId);
+                    $('#Name').val(i.Name);
+                }
+            });   
+              $.ajax({
+                url: "<?php echo base_url('Customer/GetReceipts/'); ?>" + id,
+                success: function(i){
+                    i = JSON.parse(i);
+                    console.log("edit"); 
+                    console.log(i);
+                    //$('#MenuId').val(i.MenuId);
                     $('#Quantity').val(i.Quantity);
                 }
             });     
@@ -125,11 +137,11 @@ $('#pdf').click(function () {
   var date = $('#date').val();
   var day = $('#day').val();
   var time = $('#time').val();
-  var Change = $('form#smdiv input[name="change"]').val();
-  var discount = $('form#smdiv select[name="discount"]').val();
-  var puretotal = $('form#smdiv input[name="puretotal"]').val();
-  var Cash = $('form#smdiv input[name="ReceivedAmnt"]').val();
-  var Change = $('form#smdiv input[name="change"]').val();
+//  var Change = $('#Change').val();
+  var discount = $('#Discount').val();
+  var puretotal = $('#Total').val();
+  var Cash = $('#Cash').val();
+  var Change = $('#Change').val();
   var VAT = parseFloat(puretotal * 0.12);
   var VATable = parseFloat(puretotal - VAT);
   var VATExempt;
@@ -147,22 +159,50 @@ $('#pdf').click(function () {
   }
   else{
     TotalDue = parseFloat(VATExempt * .8);
-  }
+  } 
 
 
-  var pdf = new jsPDF('p', 'mm', [400, 330]);
-  pdf.text(40, 5, 'FOODWAZE');
-  pdf.text(35, 10, 'Receipt# '+' 312312');
-  pdf.text(15, 15, time +' | '+date);
-  pdf.text(5, 25, menu);
-  pdf.text(5, 80, '------------------------------------------------------');
-  pdf.text(5, 85, 'VATable:                                  '+ VATable);
-  pdf.text(5, 90, 'VAT Exempt:                            '+ VATExempt);
-  pdf.text(5, 95, 'VAT:                                        '+ VAT);
-  pdf.text(5, 100, '-----------------------------------------------------');
-  pdf.text(5, 105, 'Total Amount Due:                    '+ TotalDue );
-  pdf.text(5, 110, 'Cash:                                         '+ Cash+'.00');
-  pdf.text(5, 115, 'Change:                                     '+ Change);
+  var pdf = new jsPDF('p', 'mm', [730, 350]);
+  pdf.text(45, 5, 'FOODWAZE Inc.');
+  pdf.text(40, 10, 'Jollibe Gen. Luna');
+  pdf.text(10, 15, 'Gen.Luna cor. UN-Taft Ave.,Metro Manila');
+  pdf.text(25, 20, 'VAT reg TIN:'+'005-571-216-002');
+  pdf.text(25, 25, 'MIN:'+'17101118105743142');
+  pdf.text(1, 30, '-----------------------------------------------------------------');
+  pdf.text(5, 50, menu);
+  pdf.text(1, 90, '----------------------------------------------------------------');
+  pdf.text(10, 95, 'VATable:                                   '+ VATable);
+  pdf.text(10, 100, 'VAT Exempt:                             '+ VATExempt);
+  pdf.text(10, 105, 'VAT:                                           '+ VAT);
+  pdf.text(10, 110, 'Total Amount Due:                    '+ TotalDue+'.00');
+  pdf.text(10, 115, 'Cash:                                         '+ Cash+'.00');
+  pdf.text(10, 120, 'Change:                                     '+ Change+'.00');
+  pdf.text(1, 125, '----------------------------------------------------------------');
+  pdf.text(5, 130, 'OR Number: 0600072035');
+  pdf.text(5, 135, 'Cashier: Rere Mariano');
+  pdf.text(5, 140, 'Date:'+'Time:');
+  pdf.text(1, 145, '----------------------------------------------------------------');
+  pdf.text(17, 150, 'This serves as your Official Receipt.');
+  pdf.text(20, 155, 'We love to hear your feedback...');
+  pdf.text(20, 160, 'Thank you, and please come again.');
+  pdf.text(35, 165, 'Call: (02) 898-777');
+  pdf.text(25, 170, 'Text only: (0917) 131-8000');
+  pdf.text(15, 175, 'Email: feedback@foodwaze.com.ph');
+  pdf.text(20, 180, 'Website: www.foodwaze.ga.ph');
+  pdf.text(1, 185, '----------------------------------------------------------------');
+  pdf.text(1, 190, 'Customer Name:________________________');
+  pdf.text(1, 195, 'Address:______________________________');
+  pdf.text(1, 200, 'TIN#:_________________________________');
+  pdf.text(1, 205, 'Bus Style:_____________________________');
+  pdf.text(20, 210, 'Sweda Systems Philippines, Inc.');
+  pdf.text(25, 215, '27f Rufino Tower, Makati City');
+  pdf.text(23, 220, 'Vat Reg TIN: 003-510-344-000');
+  pdf.text(22, 226, 'Accr No: 047-003510344-000003');
+  pdf.text(35, 231, 'Date Issued 03/10/2010');
+  pdf.text(32, 236, 'and Valid until 07/31/2020');
+  pdf.text(5, 241, 'THIS INVOICE/RECEIPT SHALL BE VALID');
+  pdf.text(10, 246, 'FIVE (5) YEARS FROM THE DATE OF');
+  pdf.text(35, 251, 'THE PERMIT TO USE');
   pdf.save('Receipt.pdf');
   console.log(menu);
   }); 
